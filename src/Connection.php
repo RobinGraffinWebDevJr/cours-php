0be73f592c2b33a6f5c4dfe45966c36b18173679
Thealiasfmr <robin.graffin@gmail.com>
2022-07-28 18:17:28 +0200

    new

--- src/Connection.php
+++ src/Connection.php
@@ -7,7 +7,7 @@ class Connection {
 
     public static function getPDO (): PDO 
     {
-        return new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', 'Diamentrix42?,', [
+        return new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', 'root', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
